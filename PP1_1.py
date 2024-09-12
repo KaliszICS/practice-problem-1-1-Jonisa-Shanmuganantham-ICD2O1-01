bcc21fe81f43bacac88fd8a1ac8c6dd1ce8f0e80
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-12 13:53:17 +0000

    second

--- PP1_1.py
+++ PP1_1.py
@@ -11,5 +11,10 @@ def q2():
 def q3():
   print (" Help Me!")
 
+<<<<<<< HEAD:PP1_1.py
 def q4:
   #Write Assignment code here
+=======
+def q4():
+  print ("3 x 3 = 9")
+>>>>>>> 8f15161 (second):PP1.py
